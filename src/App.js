c53939d26cfb929f75de334e1f522efcbe839b32
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-29 22:40:51 +0600

    Create React form and Post API and send data to the server

--- src/App.js
+++ src/App.js
@@ -13,14 +13,29 @@ function App() {
 
   const handleAddUser = (event) => {
     event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+
+    event.target.reset();
   };
 
   return (
     <div className="App">
       <form onSubmit={handleAddUser}>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" placeholder="name" />
         <br />
-        <input type="email" name="email" id="email" />
+        <input type="email" name="email" id="email" placeholder="email" />
         <br />
         <button type="submit">Add User</button>
       </form>
